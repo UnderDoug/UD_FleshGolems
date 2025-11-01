52c199fa150bb30eaa3a64b476691e0ecc977648
UnderDoug <douglas.marken@gmail.com>
2025-11-01 23:29:35 +1000

    Added blueprint files.
    Started on corpse reanimation helper.

---
+++ UnderDoug_FleshGolems_CoprseReanimationHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XRL.World.Parts
+{
+    [Serializable]
+    public class UnderDoug_FleshGolems_CoprseReanimationHelper : IScribedPart
+    {
+
+        public override bool AllowStaticRegistration()
+        {
+            return true;
+        }
+
+        public override bool WantEvent(int ID, int Cascade)
+        {
+            return base.WantEvent(ID, Cascade)
+                || ID == AnimateEvent.ID;
+        }
+
+        public override bool HandleEvent(AnimateEvent E)
+        {
+            if (ParentObject is GameObject frankenCorpse
+                && frankenCorpse == E.Object)
+            {
+                if (frankenCorpse.TryGetPart(out ConversationScript convo)
+                    && convo.ConversationID == "NewlySentientBeings")
+                {
+                    frankenCorpse.RemovePart(convo);
+                    frankenCorpse.AddPart(new ConversationScript("UnderDoug_FleshGolems NewlyReanimatedBeings"));
+                }
+                if (frankenCorpse.GetStringProperty("SourceBlueprint") is string sourceBlueprint)
+                {
+
+                }
+                frankenCorpse.Body.Rebuild("SlugWithHands");
+            }
+            return base.HandleEvent(E);
+        }
+    }
+}
