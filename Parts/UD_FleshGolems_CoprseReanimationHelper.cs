bf4c2bdc98a1ae740284fa8b73ac587af8fdb13d
UnderDoug <douglas.marken@gmail.com>
2025-11-02 04:42:31 +1000

    got Jofo Qudwufo spawning but he's not quite right.
    Got more the flesh golem configuration completed.

--- Parts/UD_FleshGolems_CoprseReanimationHelper.cs
+++ Parts/UD_FleshGolems_CoprseReanimationHelper.cs
@@ -2,39 +2,207 @@
 using System.Collections.Generic;
 using System.Text;
 
+using XRL.Language;
+using XRL.World.Parts.Mutation;
+using XRL.World.Quests.GolemQuest;
+
 namespace XRL.World.Parts
 {
     [Serializable]
     public class UD_FleshGolems_CoprseReanimationHelper : IScribedPart
     {
+        public bool IsALIVE;
+
+        public bool AlwaysAnimate;
+
+        public UD_FleshGolems_CoprseReanimationHelper()
+        {
+            IsALIVE = false;
+            AlwaysAnimate = false;
+        }
 
         public override bool AllowStaticRegistration()
         {
             return true;
         }
 
-        public override bool WantEvent(int ID, int Cascade)
+        public static bool MakeItALIVE(GameObject Corpse)
         {
-            return base.WantEvent(ID, Cascade)
-                || ID == AnimateEvent.ID;
-        }
-
-        public override bool HandleEvent(AnimateEvent E)
-        {
-            if (ParentObject is GameObject frankenCorpse
-                && frankenCorpse == E.Object)
+            if (Corpse is GameObject frankenCorpse)
             {
+                frankenCorpse.SetIntProperty("NoAnimatedNamePrefix", 1);
+                frankenCorpse.SetIntProperty("Bleeds", 1);
+                // frankenCorpse.Physics.Solid = true;
+
+                UD_FleshGolem_ReanimatedCorpse reanimatedCorpse = frankenCorpse.RequirePart<UD_FleshGolem_ReanimatedCorpse>();
+
                 if (frankenCorpse.TryGetPart(out ConversationScript convo)
                     && convo.ConversationID == "NewlySentientBeings")
                 {
                     frankenCorpse.RemovePart(convo);
                     frankenCorpse.AddPart(new ConversationScript("UD_FleshGolems NewlyReanimatedBeings"));
                 }
-                if (frankenCorpse.GetStringProperty("SourceBlueprint") is string sourceBlueprint)
-                {
 
+                Description frankenDescription = frankenCorpse.RequirePart<Description>();
+                if (frankenDescription != null)
+                {
+                    frankenDescription._Short =
+                        "Viscera and muck are brought squirming back into a horrific facsimile of life. " +
+                        "Wet slurps and gurgles escape =subject.possessive= every movement and twist the " +
+                        "gut of anyone unfortunate enough to hear it.";
                 }
-                frankenCorpse.Body.Rebuild("SlugWithHands");
+                string sourceBlueprintName = frankenCorpse.GetStringProperty("SourceBlueprint")
+                    ?? frankenCorpse.Blueprint.Replace(" Corpse", "")
+                    ?? "Trash Monk";
+
+                Mutations frankenMutations = frankenCorpse.RequirePart<Mutations>();
+
+                if (GameObjectFactory.Factory.GetBlueprintIfExists(sourceBlueprintName) is GameObjectBlueprint sourceBlueprint)
+                {
+                    if (sourceBlueprint.DisplayName() is string sourceBlueprintDisplayName)
+                    {
+                        if (frankenDescription != null
+                            && sourceBlueprint.TryGetPartParameter(nameof(Description), nameof(Description.Short), out string sourceDescription))
+                        {
+                            frankenDescription._Short += "\n\n" +
+                                "In life, this mess was " + Grammar.A(sourceBlueprintDisplayName) + ":\n" +
+                                sourceDescription;
+                        }
+                        if (frankenCorpse.GetStringProperty("CreatureName") is string sourceCreatureName)
+                        {
+                            frankenCorpse.Render.DisplayName = "corpse of " + sourceCreatureName;
+                        }
+                        else
+                        {
+                            frankenCorpse.Render.DisplayName = sourceBlueprintDisplayName + " corpse";
+                        }
+                    }
+
+                    string BleedLiquid = null;
+                    if (sourceBlueprint.Tags.ContainsKey(nameof(BleedLiquid)))
+                    {
+                        BleedLiquid = sourceBlueprint.Tags[nameof(BleedLiquid)];
+                    }
+                    if (BleedLiquid.IsNullOrEmpty())
+                    {
+                        BleedLiquid = "blood-1000";
+                    }
+                    frankenCorpse.SetStringProperty("BleedLiquid", BleedLiquid);
+
+                    if (frankenCorpse.GetStringProperty("KillerID") is string killerID
+                        && GameObject.FindByID(killerID) is GameObject killer)
+                    {
+                        frankenCorpse.SetStringProperty("KillerName", killer.GetReferenceDisplayName(Short: true));
+                    }
+
+                    Skills frankenSkills = frankenCorpse.RequirePart<Skills>();
+                    if (frankenSkills != null)
+                    {
+                        foreach (GamePartBlueprint sourceSkill in sourceBlueprint.Skills.Values)
+                        {
+                            frankenSkills.AddSkill(sourceSkill.Name);
+                        }
+                    }
+                    foreach ((string statName, Statistic sourceStat) in sourceBlueprint.Stats)
+                    {
+                        frankenCorpse.Statistics[statName] = sourceStat;
+                    }
+
+                    if (sourceBlueprint.TryGetPartParameter(nameof(Physics), nameof(Physics.Weight), out int sourceWeight))
+                    {
+                        frankenCorpse.Physics.Weight = sourceWeight;
+                    }
+
+                    if (sourceBlueprint.TryGetPartParameter(nameof(Body), nameof(Body.Anatomy), out string sourceAnatomy))
+                    {
+                        if (frankenCorpse.Body == null)
+                        {
+                            frankenCorpse.AddPart(new Body()).Anatomy = sourceAnatomy;
+                        }
+                        else
+                        {
+                            frankenCorpse.Body.Rebuild(sourceAnatomy);
+                        }
+                    }
+
+                    if (GolemBodySelection.GetBodyBlueprintFor(sourceBlueprint) is GameObjectBlueprint golemBodyBlueprint)
+                    {
+                        if (golemBodyBlueprint.TryGetPartParameter(nameof(Body), nameof(Body.Anatomy), out string golemAnatomy))
+                        {
+                            if (frankenCorpse.Body == null)
+                            {
+                                frankenCorpse.AddPart(new Body()).Anatomy = golemAnatomy;
+                            }
+                            else
+                            {
+                                frankenCorpse.Body.Rebuild(golemAnatomy);
+                            }
+                        }
+                        if (golemBodyBlueprint.TryGetPartParameter(nameof(Parts.Render), nameof(Parts.Render.Tile), out string golemTile))
+                        {
+                            frankenCorpse.Render.Tile = golemTile;
+                            UnityEngine.Debug.Log(nameof(golemTile) + ": " + golemTile);
+                        }
+                        foreach ((string statName, Statistic sourceStat) in golemBodyBlueprint.Stats)
+                        {
+                            frankenCorpse.Statistics[statName] = new(sourceStat);
+                        }
+                    }
+                    if (frankenMutations != null)
+                    {
+                        if (!sourceBlueprint.Mutations.IsNullOrEmpty())
+                        {
+                            foreach (GamePartBlueprint sourceMutation in sourceBlueprint.Mutations.Values)
+                            {
+                                frankenMutations.AddMutation(sourceMutation.Name);
+                            }
+                        }
+                        bool giveRegen = false;
+                        if (giveRegen
+                            && MutationFactory.GetMutationEntryByName("Regeneration").Class is string regenerationMutationClass)
+                        {
+                            if (frankenMutations.GetMutation(regenerationMutationClass) is not BaseMutation regenerationMutation)
+                            {
+                                frankenMutations.AddMutation(regenerationMutationClass, Level: 10);
+                                regenerationMutation = frankenMutations.GetMutation(regenerationMutationClass);
+                            }
+                            regenerationMutation.CapOverride = 10;
+
+                            if (regenerationMutation.Level < 10)
+                            {
+                                regenerationMutation.ChangeLevel(10);
+                            }
+                        }
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public override bool WantEvent(int ID, int Cascade)
+        {
+            return base.WantEvent(ID, Cascade)
+                || ID == AnimateEvent.ID
+                || ID == AfterObjectCreatedEvent.ID;
+        }
+        public override bool HandleEvent(AnimateEvent E)
+        {
+            if (!IsALIVE
+                && ParentObject == E.Object)
+            {
+                IsALIVE = MakeItALIVE(E.Object);
+            }
+            return base.HandleEvent(E);
+        }
+        public override bool HandleEvent(AfterObjectCreatedEvent E)
+        {
+            if (ParentObject == E.Object)
+            {
+                if (AlwaysAnimate && !ParentObject.HasPart<AnimatedObject>())
+                {
+                    AnimateObject.Animate(ParentObject);
+                }
             }
             return base.HandleEvent(E);
         }
---
+++ Parts/UD_FleshGolems_ReanimedCorpse.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+
+using XRL.World.Effects;
+
+namespace XRL.World.Parts
+{
+    [Serializable]
+    public class UD_FleshGolem_ReanimatedCorpse : IScribedPart
+    {
+        public string BleedLiquid;
+
+        public Dictionary<string, int> BleedLiquidPortions;
+
+        public UD_FleshGolem_ReanimatedCorpse()
+        {
+            BleedLiquid = null;
+            BleedLiquidPortions = null;
+        }
+
+        public override bool SameAs(IPart p)
+        {
+            return false;
+        }
+
+        public static bool TryGetLiquidPortion(string LiquidComponent, out (string Liquid, int Portion) LiquidPortion)
+        {
+            LiquidPortion = new("water", 0);
+            if (LiquidComponent.Contains('-'))
+            {
+                string[] liquidComponent = LiquidComponent.Split('-');
+                if (int.TryParse(liquidComponent[1], out int portion))
+                {
+                    LiquidPortion.Liquid = liquidComponent[0];
+                    LiquidPortion.Portion = portion;
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static Dictionary<string, int> GetBleedLiquids(string BleedLiquids)
+        {
+            if (BleedLiquids.IsNullOrEmpty())
+            {
+                return new();
+            }
+            Dictionary<string, int> liquids = new();
+            if (!BleedLiquids.Contains(',') && TryGetLiquidPortion(BleedLiquids, out (string Liquid, int Portion) singleLiquidPortion))
+            {
+                liquids.Add(singleLiquidPortion.Liquid, singleLiquidPortion.Portion);
+                return liquids;
+            }
+            foreach (string liquidComponent in BleedLiquids.Split(','))
+            {
+                if (TryGetLiquidPortion(liquidComponent, out (string Liquid, int Portion) liquidPortion))
+                {
+                    liquids.Add(liquidPortion.Liquid, liquidPortion.Portion);
+                }
+            }
+            return liquids;
+        }
+
+        public bool AttemptToBleed()
+        {
+            if (ParentObject is GameObject frankenCorpse
+                && !frankenCorpse.HasEffect<Bleeding>())
+            {
+                return frankenCorpse.ForceApplyEffect(new Bleeding( Damage: "1d2", SaveTarget: 999));
+            }
+            return false;
+        }
+
+        public override bool WantTurnTick()
+        {
+            return true;
+        }
+        public override void TurnTick(long TimeTick, int Amount)
+        {
+            base.TurnTick(TimeTick, Amount);
+            if (AttemptToBleed())
+            {
+                if (75.in100())
+                {
+                    ParentObject?.Bloodsplatter();
+                }
+                else
+                if (50.in100())
+                {
+                    ParentObject?.BigBloodsplatter(SelfSplatter: false);
+                }
+            }
+        }
+        public override void Register(GameObject Object, IEventRegistrar Registrar)
+        {
+            Registrar.Register("Regenera");
+            base.Register(Object, Registrar);
+        }
+        public override bool WantEvent(int ID, int cascade)
+        {
+            return base.WantEvent(ID, cascade)
+                || ID == GetDisplayNameEvent.ID
+                || ID == GetBleedLiquidEvent.ID;
+        }
+        public override bool HandleEvent(GetDisplayNameEvent E)
+        {
+            if (!E.Object.HasProperName && !E.Object.HasTagOrProperty("NoReanimatedNamePrefix"))
+            {
+                E.AddAdjective("{{UD_FleshGolem_reanimated|reanimated}}", 5);
+            }
+            return base.HandleEvent(E);
+        }
+        public override bool HandleEvent(GetBleedLiquidEvent E)
+        {
+            if (BleedLiquidPortions == null)
+            {
+                string baseBlood = E.BaseLiquid ?? E.Actor.GetStringProperty("BleedLiquid", "blood-1000");
+                BleedLiquidPortions = GetBleedLiquids(baseBlood);
+                int combinedPortions = 0;
+                foreach ((string _, int portion) in BleedLiquidPortions)
+                {
+                    combinedPortions += portion;
+                }
+                if (combinedPortions == 0)
+                {
+                    combinedPortions = 500;
+                }
+                int combinedFactor = combinedPortions / 10;
+                List<(string Liquid, int Portion)> contamination = new()
+                {
+                    ("putrid", combinedFactor * 6),
+                    ("gel", combinedFactor * 3),
+                    ("acid", combinedFactor * 1),
+                };
+                foreach ((string liquid, int portion) in contamination)
+                {
+                    if (BleedLiquidPortions.ContainsKey(liquid))
+                    {
+                        BleedLiquidPortions[liquid] += portion;
+                    }
+                    else
+                    {
+                        BleedLiquidPortions.Add(liquid, portion);
+                    }
+                }
+            }
+            if (BleedLiquid.IsNullOrEmpty())
+            {
+                LiquidVolume bleedLiquidVolume = new(BleedLiquidPortions);
+                bleedLiquidVolume.NormalizeProportions();
+                foreach ((string liquid, int portion) in bleedLiquidVolume.ComponentLiquids)
+                {
+                    if (!BleedLiquid.IsNullOrEmpty())
+                    {
+                        BleedLiquid += ",";
+                    }
+
+                    BleedLiquid += liquid + "-" + portion;
+                }
+            }
+            E.Liquid = BleedLiquid;
+            return base.HandleEvent(E);
+        }
+        public override bool FireEvent(Event E)
+        {
+            if (E.ID == "Regenera")
+            {
+                AttemptToBleed();
+            }
+            return base.FireEvent(E);
+        }
+    }
+}
